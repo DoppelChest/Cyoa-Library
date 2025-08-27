13e1b517ce9f0cc29264d1756dd4d255008c52fd
DoppelChest <bargwo000@gmail.com>
2025-08-27 18:35:07 +0200

    eye icon

--- scripts/panic_button.js
+++ scripts/panic_button.js
@@ -2,7 +2,6 @@ const toggleBtn = document.getElementById('toggleBtn');
 const mainDivs = document.querySelectorAll('.general');
 const originalTitle = document.title;   // Save the original page title
 const hiddenTitle = "Black Screen";   // Title when div is hidden
-//const panicURL = "https://en.wikipedia.org/wiki/Special:Random";
 
 toggleBtn.addEventListener('click', () => {
     mainDivs.forEach(div => {
@@ -11,13 +10,13 @@ toggleBtn.addEventListener('click', () => {
             div.style.opacity = '1';
             div.style.pointerEvents = 'auto';
             document.title = originalTitle;
+            toggleBtn.src = '../images/eye1.png'; // Show eye1 when content is visible
         } else {
             div.style.visibility = 'hidden';
             div.style.opacity = '0';
             div.style.pointerEvents = 'none';
             document.title = hiddenTitle;
-
-            //window.open(panicURL, '_blank');
+            toggleBtn.src = '../images/eye2.png'; // Show eye2 when content is hidden
         }
     });
 });
